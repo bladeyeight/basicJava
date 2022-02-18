f6dee25ea0cd0705dc69f2f8b2deb12e65c2f93f
Sam Perry <samueljperry1991@gmail.com>
2022-02-18 15:31:07 -0500

    first intellij commit

---
+++ src/com/teksystems/Main.java
@@ -0,0 +1,40 @@
+package com.teksystems;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Question 1
+        int x = 5;
+        int y = 11;
+        int sum;
+
+        sum = x+y;
+        System.out.println("Question 1: " + sum);
+//     Question 2
+
+        double m = 3.9;
+        double n = 6.8;
+        double sumDouble;
+
+        sumDouble = m + n;
+
+        System.out.println("Question 2: " + sumDouble);
+
+//     Question 3
+        int o = 6;
+        double p = 7.3;
+        double intDouble;
+
+        intDouble = o + p;
+
+        System.out.println("Question 3: " + intDouble + " intDouble has to be a double");
+//      Question 4
+        int d = 5;
+        double c = 9.4;
+        double divide;
+
+        divide = c/d;
+
+        System.out.println("Question 4: " + divide);
+    }
+}
