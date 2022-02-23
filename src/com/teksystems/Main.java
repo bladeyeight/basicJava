dd48864437e0e4261e191b9f7c2fcce9e4a32287
Sam Perry <samueljperry1991@gmail.com>
2022-02-23 17:45:16 -0500

    did some of the conditional assignment

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -44,5 +44,33 @@ public class Main {
         quotient = r/q;
 
         System.out.println("Question 5: " + quotient);
+
+//        Core Java Conditional Statements
+//        Question 1
+        int v;
+        v = 7;
+        if(v < 10){
+            System.out.println("Less than 10");
+            v = 15;
+        }
+//        Question 2
+        int v1;
+        v1 = 15;
+        if (v1 < 10){
+            System.out.println("Less than 10");
+        }else if (v1 > 10){
+            System.out.println("Greater than 10");
+        }
+
+//        Question 3
+        int v2;
+        v2 = 15;
+        if (v2 < 10){
+            System.out.println("Less than 10");
+        }else if (v2 < 20 && v2 > 10){
+            System.out.println("Between 10 and 20");
+        } else if (v2 >= 20){
+            System.out.println("Greater than or equal to 20");
+        }
     }
 }
